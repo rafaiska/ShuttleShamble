8734d4633d020522f3f81951cd7bd1778ae9488a
Rafael <paocomcarnemoidaetubaina@gmail.com>
2024-04-25 13:36:33 -0300

    Assets file tests (WIP)

---
+++ src/test/assets_file_test.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include "manager.hpp"
+
+class AssetsFileTest : public testing::Test {
+    protected:
+        GMManager* manager;
+        FileService* file_service;
+        AssetManager* asset_manager;
+
+        AssetsFileTest(){}
+
+        void SetUp() override
+        {
+            manager = GMManager::get_instance();
+            file_service = manager->start_file_service();
+            asset_manager = manager->start_asset_manager();
+        }
+
+        void TearDown() override
+        {
+            asset_manager->close_assets_file();
+        }
+};
+
+TEST_F(AssetsFileTest, create_asset_file) {
+    AssetsFile* file = asset_manager->create_assets_file("src/test/assets", "test_assets.gma");
+//  index                 88000 bytes
+//  sample.csv 45 + 118 =   163 bytes
+//  sample.png 45 + 640 =   685 bytes
+//  sample.xml 45 + 171 =   216 bytes
+//                        89064 bytes total
+    ASSERT_EQ(file->get_size(), 89064);
+}
