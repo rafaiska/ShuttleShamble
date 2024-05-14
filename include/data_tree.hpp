7c75175378d92357b19ffb1096d3d0406be4f5ea
Rafael <paocomcarnemoidaetubaina@gmail.com>
2024-05-14 20:20:57 -0300

    Data tree basics

--- include/data_tree.hpp
+++ include/data_tree.hpp
@@ -2,24 +2,27 @@
 #define _DATA_TREE_H_
 
 #include <string>
+#include <vector>
 #include <unordered_map>
 
 #include "utils.hpp"
-
-class GMDataTreeNode;
 
 class GMDataTreeNode
 {
     std::string name;
     std::string value;
     bool is_root = false;
-    std::unordered_map<std::string, GMDataTreeNode> params;
-    std::unordered_map<std::string, GMDataTreeNode> children;
+    std::unordered_map<std::string, std::string> params;
+    std::unordered_map<std::string, GMDataTreeNode*> children;
 
     public:
         GMDataTreeNode(std::string name_, std::string value_, bool is_root_): name(name_), value(value_), is_root(is_root_){}
-        GMDataTreeNode& get_child(std::string child_name);
-        GMDataTreeNode& insert_child(std::string child_name);
+        GMDataTreeNode* get_child(std::string child_name);
+        std::unordered_map<std::string, GMDataTreeNode*> get_children() {return children;}
+        GMDataTreeNode* insert_child(std::string child_name, std::string value);
+        void add_param(std::string name, std::string value);
+        std::string get_param(std::string name);
+        std::string get_name() {return name;}
 
         class ChildAlreadyExists : public GMException {public: ChildAlreadyExists(): GMException("Data Tree Node: trying to insert same child twice"){}};
         class ChildNotFound : public GMException {public: ChildNotFound(): GMException("Data Tree Node: child not found"){}};
@@ -27,10 +30,13 @@ class GMDataTreeNode
 
 class GMDataTree
 {
-    GMDataTreeNode root_node = GMDataTreeNode("ROOT", "", true);
+    GMDataTreeNode* root_node = new GMDataTreeNode("ROOT", "", true);
 
     public:
-        GMDataTreeNode& get_root() {return root_node;}
+        ~GMDataTree();
+        
+        std::vector<GMDataTreeNode*> all_nodes();
+        GMDataTreeNode* get_root() {return root_node;}
         void load_from_xml_data(char* buffer, uint32_t buffer_size);
 };
 
--- src/test/data_tree_test.cpp
+++ src/test/data_tree_test.cpp
@@ -1,22 +1,48 @@
 #include <gtest/gtest.h>
+#include <algorithm>
+#include <set>
 
 #include "data_tree.hpp"
 
 class DataTreeTest : public testing::Test {
     protected:
-        GMDataTree data_tree;
+        GMDataTree* data_tree;
 
         DataTreeTest(){}
 
         void SetUp() override {
+            data_tree = new GMDataTree();
         }
 
         void TearDown() override {
+            delete data_tree;
         }
 };
 
+TEST_F(DataTreeTest, test_all_nodes) {
+    GMDataTreeNode* amendo_node = data_tree->get_root()->insert_child("amendoim_nuts", "AN");
+    amendo_node->insert_child("passoca", "PS");
+    amendo_node->insert_child("lads_foot", "LF");
+    amendo_node->insert_child("quebraquexo", "QQ")->insert_child("caramel", "CM");
+
+    std::set<std::string> node_names;
+    for (GMDataTreeNode* node: data_tree->all_nodes())
+    {
+        node_names.insert(node->get_name());
+    }
+
+    ASSERT_EQ(node_names.size(), 6);
+    ASSERT_TRUE(node_names.count("ROOT"));
+    ASSERT_TRUE(node_names.count("amendoim_nuts"));
+    ASSERT_TRUE(node_names.count("passoca"));
+    ASSERT_TRUE(node_names.count("lads_foot"));
+    ASSERT_TRUE(node_names.count("quebraquexo"));
+    ASSERT_TRUE(node_names.count("caramel"));
+}
+
 TEST_F(DataTreeTest, insert_nodes_into_tree) {
-    data_tree.get_root().insert_child("passoca");
-    data_tree.get_root().get_child("passoca").insert_child("amendoim");
-    data_tree.get_root().get_child("passoca").get_child("amendoim");
+    data_tree->get_root()->insert_child("passoca", "PS")->insert_child("amendoim", "AD")->add_param("qt", "101");
+    GMDataTreeNode* node = data_tree->get_root()->get_child("passoca")->get_child("amendoim");
+
+    ASSERT_EQ(node->get_param("qt"), "101");
 }
