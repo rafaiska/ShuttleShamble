f9f513b4a90902431f9c1612a83b08cd296ecb6e
rafaiska <paocomcarnemoidaetubaina@gmail.com>
2026-06-21 21:14:31 -0300

    SDL1.2 8 bit surface

--- src/test/teste_maroto.cpp
+++ src/test/teste_maroto.cpp
@@ -1,27 +1,41 @@
-#include <SDL2/SDL.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_video.h>
+#include <chrono>
+#include <thread>
 
 void setUpSurface(SDL_Surface* surface) {
 
 }
 
-void drawImage(SDL_Surface* surface) {
+void drawImage(SDL_Surface* surface, uint8_t delta) {
     for(int i = 0; i < 144; i++)
         for(int j = 0; j < 160; j++)
-            ((uint8_t*)surface->pixels)[(160*i) + j] = (uint8_t)((i + 1) / 36);
+            ((uint8_t*)surface->pixels)[(160*i) + j] = i + delta;
 }
 
 int main(int argc, char* argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("Maroto", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 160, 144, SDL_WINDOW_SHOWN);
-    SDL_Surface* surface = SDL_GetWindowSurface(window);
+    SDL_SetVideoMode(160, 144, 8, 0);
+    SDL_Surface* surface = SDL_GetVideoSurface();
 
     setUpSurface(surface);
-    drawImage(surface);
-    SDL_UpdateWindowSurface(window);
 
-    SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+    SDL_Event e;
+    bool quit = false;
+    uint8_t delta = 0;
+    while( quit == false )
+    { 
+        while( SDL_PollEvent( &e ) )
+        { 
+            if( e.type == SDL_QUIT ) 
+                quit = true; 
+        }
+        drawImage(surface, delta++);
+        SDL_UpdateRect(surface, 0, 0, 160, 144);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
 
     SDL_Quit();
 }
